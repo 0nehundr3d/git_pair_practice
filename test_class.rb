86031fe5f0dd52fe882ea3fcffbd109d2c5e0d02
Sebastian McKee <sebmcke@gmail.com>
2024-12-19 10:43:19 -0600

    Change test class to Test2

--- test_class.rb
+++ test_class.rb
@@ -1,8 +1,9 @@
-class Test
-    attr_reader :int, :str
+class Test2
+    attr_reader :int, :str, :int2
 
     def initialize(int, str)
         @int = int 
         @str = str
+        @int1 = @int*12
     end
 end
